e470ec563bef8a530576e0a36a0b5a581da874a5
Nick Roberts <nick.roberts@smartfleetsolutions.com>
2021-10-26 17:00:14 +0100

    Roman Numeral Kata

--- src/main/example.ts
+++ src/main/example.ts
@@ -1,5 +1,37 @@
-export class Example {
-    add(a: number, b: number) {
-        return a + b;
+export function romanNumber(input : number) : string {
+    let romanString = '';  
+    
+    if(input >= 50){
+        romanString += 'L';
+        input = input - 50;
     }
+
+    while(input > 9 && input < 40){
+        romanString += 'X';
+        input = input - 10;
+    }
+
+    if(input >= 5 && input < 9){
+        romanString += 'V';
+        input = input -5;
+    }    
+    
+    while (input > 0 && input < 4) {
+        romanString += 'I';
+        input --;
+   }
+
+   if(input === 4){
+       romanString += 'IV';
+   }
+
+   if(input === 9){
+       romanString += 'IX';
+   }
+
+   if(input === 40){
+      romanString += 'XL';
+    }   
+   
+   return romanString;   
 }
--- src/test/example.test.ts
+++ src/test/example.test.ts
@@ -1,8 +1,36 @@
-import {Example} from "../main/example";
+import { romanNumber } from "../main/example";
 
-describe('example test', () => {
-    it("should give sum of a + b when adding a and b", () => {
-        let example: Example = new Example();
-        expect(example.add(1, 2)).toBe(3);
+describe('roman numbers test', () => {
+    it.each(
+        [
+            ['' , 0],
+            ['I', 1],
+            ['II', 2],
+            ['III', 3],
+            ['IV', 4],
+            ['V', 5],
+            ['VI', 6],
+            ['VII', 7],
+            ['VIII', 8],
+            ['IX', 9],
+            ['X', 10],
+            ['XI', 11],
+            ['XII', 12],
+            ['XIII', 13],
+            ['XIV', 14],
+            ['XV', 15],
+            ['XVI', 16],
+            ['XVII', 17],
+            ['XVIII', 18],
+            ['XIX', 19],
+            ['XX', 20],
+            ['XXXIX', 39],
+            ['XL', 40],
+            ['L', 50],
+            ['LI', 51]
+        ]
+    )   ("should return %s string when given %i", (roman : string, arabic : number) => {
+        expect(romanNumber(arabic)).toEqual(roman);
     })
 })
+
